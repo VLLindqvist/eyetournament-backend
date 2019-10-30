003f55fb278a16e96846ba44d4f6aa1b10cd0916
vicli268 <vicli268@student.liu.se>
2019-10-30 18:35:01 +0100

    hej

--- functionClasses/content.js
+++ functionClasses/content.js
@@ -1,22 +1,42 @@
 'use strict';
 
 class Content {
-    constructor(page, content, lang, newContent){
+    constructor(page, content, lang, html, title = null, lead = null, body = null){
         this.page = page;
         this.content = content;
         this.lang = lang;
-        this.newContent = newContent;
-
-        this["edit_" + page]();
+        this.html = html;
+        this.title = title;
+        this.lead = lead;
+        this.body = body;
     }
 
-    edit_about() {
+    async post_about() {
+      if(this.html !== null) {
+        const newContent = {
+          title: null,
+          lead: null,
+          body: null,
+          html: this.html
+        };
 
-    }
+        this.render(await this.db.edit('main', {type: "about"}, null, { lang: data }));
+      }
 
-    async getContent() {
+      if(this.title !== null && this.lead !== null && this.body !== null) {
+        this.content.html = null;
+      }
+
       return this.content;
     }
+
+    async edit_about(which) {
+      return this.content;
+    }
+
+    // async getContent() {
+    //   return this.content;
+    // }
 
 }
 
--- src/about.js
+++ src/about.js
@@ -11,7 +11,9 @@ class About extends Library {
 
     async index(){
       if(this.method == 'GET'){this.self(); return;}
-      if(this.method == 'POST'){this.update(); return;}
+      if(this.method == 'POST'){this.newPost(); return;}
+      if(this.method == 'PATCH'){this.update(); return;}
+      if(this.method == 'DELETE'){this.update(); return;}
       this.render({status: false}, 404);
     }
 
@@ -21,17 +23,25 @@ class About extends Library {
       this.render(data.lang[lang], 200); return;
     }
 
-    async update() {
+    async newPost() {
       const user = await this.authenticate();
       if(!user){this.render({status: false, error: "unauthorized"}, 401); return;}
 
-      if(user.group == "Admin") { this.render({status: false, error: "cant touch this"}, 401); return; }
+      if(user.group !== "Admin") { this.render({status: false, error: "cant touch this"}, 401); return; }
 
       const input = await this.post();
-      if(this.isset(input, ["content", "lang"])) {
+      if(this.isset(input, ["html", "lang"])) {
         let data = await this.db.find('main', {type: "about"}, {about: 1, _id: 0});
-        const content = await new Content("about", data, input.lang, input.content);
-        const newContent = await content.getContent();
+        const content = await new Content("about", data, input.lang, input.html);
+        const newContent = await content.post_about();
+
+        this.render(await this.db.edit('main', {type: "about"}, newContent), 200); return;
+      }
+
+      if(this.isset(input, ["title", "lead", "body", "lang"])) {
+        let data = await this.db.find('main', {type: "about"}, {about: 1, _id: 0});
+        const content = await new Content("about", data, input.lang, null, input.title, input.lead, input.body);
+        const newContent = await content.post_about();
 
         this.render(await this.db.edit('main', {type: "about"}, newContent), 200); return;
       }
